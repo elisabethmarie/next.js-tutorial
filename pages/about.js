4cdca8a1760b72d7ed19429a22115b6c2d83dea6
Elisabeth <32246797+elisabethmarie@users.noreply.github.com>
2019-03-21 16:09:57 +0100

    add files

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/mylayout.js'
+export default function About() {
+  return (
+    <div>
+      <Layout>
+
+      <p>Hello Next.js</p>
+      </Layout>
+    </div>
+  )
+}
